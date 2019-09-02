d9a7332c6ab4af3ee7c7046a0c2f5509e567864b
Naveed Ganjani <nganjani@protonmail.com>
2019-09-02 17:32:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,5 @@
 def find_element_index(array, value_to_find)
-  if array.include?(value_to_find)
-    return array.index(value_to_find)
-  end
+  return array.index(value_to_find) if array.include?(value_to_find)
 end
 
 def find_max_value(array)
